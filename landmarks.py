84436b4f3fdb6174aea6fa1f074ea296448d4968
rain <5238832+rainfly123@user.noreply.gitee.com>
2020-09-10 09:36:51 +0800

    adf

--- landmarks.py
+++ landmarks.py
@@ -5,11 +5,11 @@ import cv2
 img = cv2.imread(sys.argv[1])
 img_rd = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('data/data_dlib/shape_predictor_68_face_landmarks.dat')
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 faces = detector(img_rd, 0)
-print (len(faces)):
+print (len(faces))
 shape = predictor(img_rd, faces[0])
 
 for i in range(68):
-    print(shape.part[i].x, shape.part[i].y)
+    print(i, shape.part(i).x, shape.part(i).y)
 
