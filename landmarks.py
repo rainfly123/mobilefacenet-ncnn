8a94a6d5d0f1d76fa16b50919f5f8136ec4ddc35
rain <5238832+rainfly123@user.noreply.gitee.com>
2020-09-10 10:32:16 +0800

    adf

--- landmarks.py
+++ landmarks.py
@@ -2,14 +2,25 @@ import dlib
 import sys
 import cv2
 
-img = cv2.imread(sys.argv[1])
-img_rd = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
-faces = detector(img_rd, 0)
-print (len(faces))
-shape = predictor(img_rd, faces[0])
+def landmarks(img_rd):
+    img_rd = cv2.resize(img_rd, (112, 112))
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    faces = detector(img_rd, 0)
+    total  = len(faces)
+    loc = list()
+    for i in range(total) :
+        shape = predictor(img_rd, faces[i])
+        points = [
+                 [shape.part(37).x + 2, shape.part(37).y + 2],
+                 [shape.part(43).x + 2, shape.part(43).y + 2],
+                 [shape.part(30).x, shape.part(30).y],
+                 [shape.part(48).x + 2 , shape.part(48).y],
+                 [shape.part(54).x + 2 , shape.part(54).y]]
+        loc.append(points)
+    return total, loc
 
-for i in range(68):
-    print(i, shape.part(i).x, shape.part(i).y)
-
+if __name__ == "__main__":
+    img_rd = cv2.imread(sys.argv[1])
+    total, loc = landmarks(img_rd)
+    print(total, loc)
